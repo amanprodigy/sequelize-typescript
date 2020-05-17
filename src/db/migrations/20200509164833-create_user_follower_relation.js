b96708b8fb0977008f62770957396916237e89db
Devansh Kumar <devansh3790@outlook.com>
2020-05-17 16:48:14 +0530

    Merge into single migration

--- src/db/migrations/20200509164833-create_user_follower_relation.js
+++ src/db/migrations/20200509164833-create_user_follower_relation.js
@@ -1,39 +1,65 @@
 "use strict";
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("user_follower", {
-      id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      user_id: {
-        type: Sequelize.UUID,
-        field: "user_id",
-        allowNull: false,
-        comment: "User id of the user who is getting followed"
-      },
-      follower_id: {
-        type: Sequelize.UUID,
-        field: "follower_id",
-        allowNull: false,
-        comment: "User id of the user who is following"
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      deleted_at: {
-        allowNull: true,
-        type: Sequelize.DATE
-      }
-    });
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.createTable("user_follower", {
+        id: {
+          type: Sequelize.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        user_id: {
+          type: Sequelize.UUID,
+          field: "user_id",
+          allowNull: false,
+          comment: "User id of the user who is getting followed"
+        },
+        follower_id: {
+          type: Sequelize.UUID,
+          field: "follower_id",
+          allowNull: false,
+          comment: "User id of the user who is following"
+        },
+        created_at: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updated_at: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        deleted_at: {
+          allowNull: true,
+          type: Sequelize.DATE
+        }
+      });
+      await queryInterface.addIndex(
+        'user_follower',
+        {
+          fields: ['user_id', 'follower_id'],
+          unique: true,
+          transaction,
+        }
+      );
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+    return
   },
 
-  down: (queryInterface, Sequelize) => queryInterface.dropTable("user_follower")
+  async down (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.removeIndex('user_follower', ['user_id', 'follower_id'], { transaction });
+      await queryInterface.dropTable("user_follower")
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+    
+  } 
 };
