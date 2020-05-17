4f733f44742066fcb43cbd264bd8306980bd1813
Devansh Kumar <devansh3790@outlook.com>
2020-05-17 13:06:04 +0530

    added down/undo migration

--- src/db/migrations/20200517064039-user-follower-composite-unique-constraint.js
+++ src/db/migrations/20200517064039-user-follower-composite-unique-constraint.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -9,23 +9,23 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-   const transaction = await queryInterface.sequelize.transaction();
-   try {
-    await queryInterface.addIndex(
-      'user_follower',
-      {
-        fields: ['user_id','follower_id'],
-        unique: true,
-        transaction,
-      }
-    );
-   } catch (error) {
-    await transaction.rollback();
-    throw error;
-   }
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.addIndex(
+        'user_follower',
+        {
+          fields: ['user_id', 'follower_id'],
+          unique: true,
+          transaction,
+        }
+      );
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
   },
 
-  down: (queryInterface, Sequelize) => {
+  async down (queryInterface, Sequelize) {
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
@@ -33,5 +33,13 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.removeIndex('user_follower', ['user_id', 'follower_id'], { transaction });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
   }
 };
